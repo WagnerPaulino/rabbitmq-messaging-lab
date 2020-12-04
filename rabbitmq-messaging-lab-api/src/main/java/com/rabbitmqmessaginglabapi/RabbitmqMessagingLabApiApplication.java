67747b6d2900f1042131a426013e1a99665e2ca2
Wagner Paulino <wagnersantos9778@gmail.com>
2020-12-03 22:39:19 -0300

    api - first commit

---
+++ rabbitmq-messaging-lab-api/src/main/java/com/rabbitmqmessaginglabapi/RabbitmqMessagingLabApiApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmqmessaginglabapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqMessagingLabApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqMessagingLabApiApplication.class, args);
+	}
+
+}
---
+++ rabbitmq-messaging-lab-api/src/test/java/com/rabbitmqmessaginglabapi/RabbitmqMessagingLabApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmqmessaginglabapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqMessagingLabApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
